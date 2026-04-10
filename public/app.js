b8bde441ed1c05e03d2da5b0570bd76a4e63f192
cobalain471-blip <cobalain471@gmail.com>
2026-04-10 16:48:39 +0700

    Refactor payment process functions in app.js

--- public/app.js
+++ public/app.js
@@ -1,11 +1,36 @@
-function openLock() {
+let totalBayar = 0;
+
+function openInfo() {
   fetch("/klik-thumbnail", { method: "POST" });
 
-  document.getElementById("popup").classList.remove("hidden");
+  document.getElementById("infoBox").classList.remove("hidden");
 }
 
-function bayar() {
-  fetch("/klik-bayar", { method: "POST" });
+function lanjutBayar() {
+  document.getElementById("infoBox").classList.add("hidden");
 
-  window.location.href = "/success";
+  // generate kode unik
+  let unik = Math.floor(Math.random() * 900) + 100;
+  totalBayar = 5000 + unik;
+
+  document.getElementById("nominal").innerText =
+    "Transfer sebesar: Rp " + totalBayar;
+
+  document.getElementById("qrisBox").classList.remove("hidden");
+}
+
+function konfirmasi() {
+  document.getElementById("qrisBox").classList.add("hidden");
+  document.getElementById("confirmBox").classList.remove("hidden");
+}
+
+function cekBayar() {
+  const input = document.getElementById("inputNominal").value;
+
+  if (parseInt(input) === totalBayar) {
+    fetch("/klik-bayar", { method: "POST" });
+    window.location.href = "/success";
+  } else {
+    alert("Nominal tidak sesuai!");
+  }
 }
