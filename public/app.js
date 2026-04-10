98f503cc7c6452a7c2f55724cf1d074328f75c25
cobalain471-blip <cobalain471@gmail.com>
2026-04-10 16:51:00 +0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,35 +1,16 @@
-let totalBayar = 0;
-
-function openInfo() {
-  fetch("/klik-thumbnail", { method: "POST" });
-
-  document.getElementById("infoBox").classList.remove("hidden");
-}
-
-function lanjutBayar() {
-  document.getElementById("infoBox").classList.add("hidden");
-
-  // generate kode unik
-  let unik = Math.floor(Math.random() * 900) + 100;
-  totalBayar = 5000 + unik;
-
-  document.getElementById("nominal").innerText =
-    "Transfer sebesar: Rp " + totalBayar;
-
-  document.getElementById("qrisBox").classList.remove("hidden");
-}
-
-function konfirmasi() {
-  document.getElementById("qrisBox").classList.add("hidden");
-  document.getElementById("confirmBox").classList.remove("hidden");
-}
-
 function cekBayar() {
   const input = document.getElementById("inputNominal").value;
 
   if (parseInt(input) === totalBayar) {
     fetch("/klik-bayar", { method: "POST" });
-    window.location.href = "/success";
+
+    // ambil link redirect dari server
+    fetch("/api/redirect")
+      .then(res => res.json())
+      .then(data => {
+        window.location.href = data.link;
+      });
+
   } else {
     alert("Nominal tidak sesuai!");
   }
